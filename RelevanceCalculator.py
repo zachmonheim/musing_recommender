e37b2caf9d288e89c21d3aa270e922fe7395e664
zachmonheim <45493027+zachmonheim@users.noreply.github.com>
2019-06-13 12:22:02 +0200

    Update RelevanceCalculator.py

--- RelevanceCalculator.py
+++ RelevanceCalculator.py
@@ -6,67 +6,31 @@ Created on May 31, 2019
 
 @author: Zach Monheim
 '''
-import random
 import math
-import numpy
 from pip._vendor.msgpack.fallback import xrange
+import pickle
 import collections
+from itertools import product
 
-#create list of 1000 keywords - change number to adjust amount of keywords
-keywords = []
-keys = 0
-while (keys < 1000):
-    keywords.append(keys)
-    keys += 1
+#loading object from file
+def load_obj(name ):
+    with open(name + '.pkl', 'rb') as f:
+        return pickle.load(f)
 
-#create a dataset with 1000 user IDs and assign a list of random keyword IDs based on a list of keywords
-#represents the user profiles
-userIDs = []
+#loads userIDs matrix
+userIDs = load_obj("user_IDs")
 
-#change second number to change number of users
-for i in xrange(0,1000):
-    #change second number to adjust max number of keywords any user may have
-    userAmount = random.randint(1, 15)
-    x = random.sample(keywords, userAmount)
-    userIDs.append(x)
+#loads videoIDs matrix
+videoIDs = load_obj("item_IDs")
 
+#loads dictionary from file
+user_d = load_obj("user_dict")
 
-#creates a dictionary out of the userIDs matrix to keep track of IDs throughout
-user_d = dict()
-key = 0
-for i in userIDs:
-    user_d.setdefault(key, i)
-    key += 1
+#loads dictionary from file
+item_d = load_obj("item_dict")
 
-
-#create a dataset with 1000 item IDs and assign a list of random keyword IDs based on a list of keywords
-#represents the tags on a video
-itemIDs = []
-
-
-#change second number to change number of videos
-for i in xrange(0,1000):
-    #change second number to adjust max number of keywords any video may have
-    itemAmount = random.randint(1, 5)
-    x = random.sample(keywords, itemAmount)
-    itemIDs.append(x)
-
-
-#creates a dictionary out of the itemIDs matrix
-item_d = dict()
-key = 0
-for i in itemIDs:
-    item_d.setdefault(key, i)
-    key += 1
-
-
-#generate matrix of seen and not seen
-#rows are user id's and columns are video id's
-rows = len(userIDs)
-cols = len(itemIDs)
-#weight 1:9 the unseen to seen
-seenUnseen = numpy.random.choice([x for x in xrange(0, 2, 1)], rows*cols, p=[0.9, 0.1])
-seenUnseen.resize(rows,cols)
+#loads matrix from file
+seenUnseen = load_obj("seenUnseen_matrix")
 
 
 '''

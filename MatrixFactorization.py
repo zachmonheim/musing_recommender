6baa0537cba6a9d595b024c3bca19f7ed6e0261a
zachmonheim <45493027+zachmonheim@users.noreply.github.com>
2019-06-27 17:20:55 +0200

    Update MatrixFactorization.py

--- MatrixFactorization.py
+++ MatrixFactorization.py
@@ -102,18 +102,20 @@ class MF():
         """
         return self.b + self.b_u[:,np.newaxis] + self.b_i[np.newaxis:,] + self.P.dot(self.Q.T)
     
-    
-R = np.array([
-    [5, 3, 0, 1],
-    [4, 0, 0, 1],
-    [1, 1, 0, 5],
-    [1, 0, 0, 4],
-    [0, 1, 5, 4],
-])
-
-mf = MF(R, K=2, alpha=0.1, beta=0.01, iterations=20)
 
 
+#loading object from file
+def load_obj(name ):
+    with open(name + '.pkl', 'rb') as f:
+        return pickle.load(f)
+
+#loads score matrix
+score_matrix = load_obj("score_matrix")
+
+#here you can change the K value in factorization
+mf = MF(score_matrix, K=2, alpha=0.1, beta=0.01, iterations=20)
+
+#rounds values to 3 decimal points
 np.set_printoptions(precision=3)
 print(mf.full_matrix())
 

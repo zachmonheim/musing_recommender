9293fb43ab27d0d0e1ddf88be47d5d0aa6b14a7d
zachmonheim <45493027+zachmonheim@users.noreply.github.com>
2019-07-05 14:59:03 +0200

    Update LightFMAttempt.py

--- LightFMAttempt.py
+++ LightFMAttempt.py
@@ -5,290 +5,8 @@ Created on Jun 13, 2019
 
 @author: Zach Monheim
 '''
-'''
-import numpy as np
-#uses dataset of movielens
-from lightfm.datasets import fetch_movielens
-from lightfm import LightFM
-from lightfm.evaluation import precision_at_k
 
-
-''
-data = fetch_movielens(min_rating=4.0)
-
-print(repr(data['train']))
-print(repr(data['test']))
-
-
-#instantiate and train the model
-model = LightFM(loss='warp')
-model.fit(data['train'], epochs=30, num_threads=2)
-
-#evaluate the trained model
-test_precision = precision_at_k(model, data['test'], k=5).mean()
-'''
-'''
-model = LightFM(loss = 'warp')
-
-model.fit(data['train'], epochs=30, num_threads=2)
-
-def sample_recommendation(model, data, user_ids):
-    n_users, n_items = data['train'].shape
-    for user_id in user_ids:
-        known_positives = data['item_labels'][data['train'].tocsr()                                    
-                          [user_id].indices]
-        
-        scores = model.predict(user_id, np.arange(n_items))
-
-        top_items = data['item_labels'][np.argsort(-scores)]
-
-        print("User %s" % user_id)
-        print(" Known positives:")
-        
-        for x in known_positives[:3]:
-            print(" %s" % x)
-        
-        print(" Recommended:")
-        
-        for x in top_items[:3]:
-            print(" %s" % x)
-
-
-sample_recommendation(model, data, [3, 25, 451])
-''
-
-from lightfm.datasets import fetch_stackexchange
-
-data = fetch_stackexchange('crossvalidated',
-                           test_set_fraction=0.1,
-                           indicator_features=False,
-                           tag_features=True)
-
-train = data['train']
-test = data['test']
-
-# Set the number of threads; you can increase this
-# ify you have more physical cores available.
-NUM_THREADS = 2
-NUM_COMPONENTS = 30
-NUM_EPOCHS = 3
-ITEM_ALPHA = 1e-6
-
-# Let's fit a WARP model: these generally have the best performance.
-model = LightFM(loss='warp',
-                item_alpha=ITEM_ALPHA,
-               no_components=NUM_COMPONENTS)
-
-import time
-
-# Run 3 epochs and time it.
-model = model.fit(train, epochs=NUM_EPOCHS, num_threads=NUM_THREADS)
-seconds=time.time()
-print(seconds)
-
-# Import the evaluation routines
-from lightfm.evaluation import auc_score
-
-# Compute and print the AUC score
-train_auc = auc_score(model, train, num_threads=NUM_THREADS).mean()
-print('Collaborative filtering train AUC: %s' % train_auc)\
-
-# We pass in the train interactions to exclude them from predictions.
-# This is to simulate a recommender system where we do not
-# re-recommend things the user has already interacted with in the train
-# set.
-test_auc = auc_score(model, test, train_interactions=train, num_threads=NUM_THREADS).mean()
-print('Collaborative filtering test AUC: %s' % test_auc)
-
-# Set biases to zero
-model.item_biases *= 0.0
-
-test_auc = auc_score(model, test, train_interactions=train, num_threads=NUM_THREADS).mean()
-print('Collaborative filtering test AUC: %s' % test_auc)
-
-
-
-item_features = data['item_features']
-tag_labels = data['item_feature_labels']
-
-print('There are %s distinct tags, with values like %s.' % (item_features.shape[1], tag_labels[:3].tolist()))
-
-
-# Define a new model instance
-model = LightFM(loss='warp',
-                item_alpha=ITEM_ALPHA,
-                no_components=NUM_COMPONENTS)
-
-# Fit the hybrid model. Note that this time, we pass
-# in the item features matrix.
-model = model.fit(train,
-                item_features=item_features,
-                epochs=NUM_EPOCHS,
-                num_threads=NUM_THREADS)
-
-
-# Don't forget the pass in the item features again!
-train_auc = auc_score(model,
-                      train,
-                      item_features=item_features,
-                      num_threads=NUM_THREADS).mean()
-print('Hybrid training set AUC: %s' % train_auc)
-
-
-test_auc = auc_score(model,
-                    test,
-                    train_interactions=train,
-                    item_features=item_features,
-                    num_threads=NUM_THREADS).mean()
-print('Hybrid test set AUC: %s' % test_auc)
-
-
-def get_similar_tags(model, tag_id):
-    # Define similarity as the cosine of the angle
-    # between the tag latent vectors
-    
-    # Normalize the vectors to unit length
-    tag_embeddings = (model.item_embeddings.T
-                      / np.linalg.norm(model.item_embeddings, axis=1)).T
-    
-    query_embedding = tag_embeddings[tag_id]
-    similarity = np.dot(tag_embeddings, query_embedding)
-    most_similar = np.argsort(-similarity)[1:4]
-    
-    return most_similar
-
-
-for tag in (u'bayesian', u'regression', u'survival'):
-    tag_id = tag_labels.tolist().index(tag)
-    print('Most similar tags for %s: %s' % (tag_labels[tag_id],
-                                            tag_labels[get_similar_tags(model, tag_id)]))
-
-
-
-'''
-'''
-training the movielens example
-''
-
-import numpy as np
-
-from lightfm.datasets import fetch_movielens
-
-movielens = fetch_movielens()
-
-
-for key, value in movielens.items():
-    print(key, type(value), value.shape)
-
-train = movielens['train']
-test = movielens['test']
-
-
-from lightfm import LightFM
-from lightfm.evaluation import precision_at_k
-from lightfm.evaluation import auc_score
-
-model = LightFM(learning_rate=0.05, loss='bpr')
-model.fit(train, epochs=10)
-
-train_precision = precision_at_k(model, train, k=10).mean()
-test_precision = precision_at_k(model, test, k=10).mean()
-
-train_auc = auc_score(model, train).mean()
-test_auc = auc_score(model, test).mean()
-
-print('Precision: train %.2f, test %.2f.' % (train_precision, test_precision))
-print('AUC: train %.2f, test %.2f.' % (train_auc, test_auc))
-
-model = LightFM(learning_rate=0.05, loss='warp')
-
-model.fit_partial(train, epochs=10)
-
-train_precision = precision_at_k(model, train, k=10).mean()
-test_precision = precision_at_k(model, test, k=10).mean()
-
-train_auc = auc_score(model, train).mean()
-test_auc = auc_score(model, test).mean()
-
-print('Precision: train %.2f, test %.2f.' % (train_precision, test_precision))
-print('AUC: train %.2f, test %.2f.' % (train_auc, test_auc))
-
-
-
-
-
-
-
-''
-building dataset using xml zipped file
-currently works with goodbooks-10k example dataset
-
-import os
-import zipfile
-import csv
-
-import requests
-
-
-def _download(url: str, dest_path: str):
-
-    req = requests.get(url, stream=True)
-    req.raise_for_status()
-
-    with open(dest_path, "wb") as fd:
-        for chunk in req.iter_content(chunk_size=2 ** 20):
-            fd.write(chunk)
-
-
-def get_data():
-
-    ratings_url = ("http://www2.informatik.uni-freiburg.de/" "~cziegler/BX/BX-CSV-Dump.zip")
-
-    if not os.path.exists("data"):
-        os.makedirs("data")
-
-        _download(ratings_url, "data/data.zip")
-
-    with zipfile.ZipFile("data/data.zip") as archive:
-        return (
-            csv.DictReader(
-                (x.decode("utf-8", "ignore") for x in archive.open("BX-Book-Ratings.csv")),
-                delimiter=";",
-            ),
-            csv.DictReader(
-                (x.decode("utf-8", "ignore") for x in archive.open("BX-Books.csv")), delimiter=";"
-            ),
-        )
-
-
-def get_ratings():
-
-    return get_data()[0]
-
-
-def get_book_features():
-
-    return get_data()[1]
-'''
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#attempt at creating dataset with lightFM Dataset class
 '''
 featList = ['user_keywords', 'video_keywords', 'score']
 itemid = ['1', '2', '3', '4', '5', '6']
@@ -334,11 +52,10 @@ ratingsDataset.build_user_features(videoids, scoreTuple)
 
 
 print(ratingsDataset)
-''
-
-
-
 '''
+
+
+
 
 import os
 
@@ -356,7 +73,9 @@ from lightfm import LightFM
 
 from lightfm.data import Dataset
 
-''
+
+
+#example of downloading files into LightFM dataset
 def _download(url: str, dest_path: str):
     req = requests.get(url, stream=True)
     req.raise_for_status()
@@ -474,22 +193,7 @@ model.fit(interactions, item_features=item_features)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-''
-
-
+#trying to put own csv files into lightFM dataset
 
 def get_own_data():
     
@@ -585,4 +289,4 @@ model = LightFM(loss='bpr')
 
 model.fit(own_interactions, item_features=item_features)
 
-''
+
